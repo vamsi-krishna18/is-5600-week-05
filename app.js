6fa44b732fb250bd2abf3896681382e23da3558a
vamsi-krishna18 <90308532+vamsi-krishna18@users.noreply.github.com>
2025-04-19 05:39:18 +0000

    WEEK 5 LAB

--- app.js
+++ app.js
@@ -21,4 +21,40 @@ app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
+// app.js
 
+// ...
+
+app.get('/orders', api.listOrders)
+app.get('/orders/', api.createOrder)
+// api.js
+
+/**
+ * Create an order
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function createOrder (req, res, next) {
+    const order = await Orders.create(req.body)
+    res.json(orders)
+  }
+  
+  /**
+   * List orders
+   * @param {Request} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   */
+  async function listOrders (req, res, next) {
+    const { offset = 0, limit = 25, productId, status } = req.query
+  
+    const orders = await Orders.list({ 
+      offset: Number(offset), 
+      limit: Number(limit),
+      productId, 
+      status 
+    })
+  
+    res.json(orders)
+  }
--- products.js
+++ products.js
@@ -47,3 +47,109 @@ module.exports = {
   list,
   get
 }
+// products.js
+const cuid = require('cuid')
+
+const db = require('./db')
+
+// Define our Product Model
+const Product = db.model('Product', {
+  _id: { type: String, default: cuid },
+  description: { type: String },
+  alt_description: { type: String },
+  likes: { type: Number, required: true },
+  urls: {
+    regular: { type: String, required: true },
+    small: { type: String, required: true },
+    thumb: { type: String, required: true },
+  },
+  links: {
+    self: { type: String, required: true },
+    html: { type: String, required: true },
+  },
+  user: {
+    id: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String },
+    portfolio_url: { type: String },
+    username: { type: String, required: true },
+  },
+  tags: [{
+    title: { type: String, required: true },
+  }], 
+})
+// products.js
+/**
+ * Create a new product
+ * @param {Object} product
+ * @returns {Promise<Object>}
+ */
+async function create (fields) {
+  const product = await new Product(fields).save()
+  return product
+}
+// products.js
+/**
+ * List products
+ * @param {Object} query
+ * @returns {Promise<Object[]>}
+ */
+async function list (options = {}) {
+  const { offset = 0, limit = 25, tag } = options
+
+  // Use a ternary statement to create the query object. Then pass 
+  // the query object to Mongoose to filter the products
+  const query = tag ? {
+    tags: {
+      $elemMatch: {
+        title: tag
+      }
+    }
+  } : {}
+  const products = await Product.find(query)
+    .sort({ _id: 1 })
+    .skip(offset)
+    .limit(limit)
+    
+  return products
+}
+// products.js
+
+/**
+ * Get a product
+ * @param {String} _id
+ * @returns {Promise<Object>}
+ */
+async function get (_id) {
+  const product = await Product.findById(_id)
+  return product
+}
+// products.js
+
+/**
+ * Edit a product
+ * @param {String} _id
+ * @param {Object} change
+ * @returns {Promise<Object>}
+ */
+async function edit (_id, change) {
+  const product = await get(_id)
+
+  // todo can we use spread operators here?
+  Object.keys(change).forEach(function (key) {
+    product[key] = change[key]
+  })
+  
+  await product.save()
+
+  return product
+}
+
+/**
+ * Delete a product
+ * @param {String} _id
+ * @returns {Promise<Object>}
+ */
+async function destroy (_id) {
+  return await Product.deleteOne({_id})
+}
